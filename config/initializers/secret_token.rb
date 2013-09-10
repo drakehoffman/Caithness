210aee167d740ed07fb20be61c92ce3e66b01423
Drake Hoffman <drakephoffman@gmail.com>
2013-09-10 15:25:18 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Caithness::Application.config.secret_token = '783f6d5ff3ac9cfbaa03bb9011acfdab30a8e0f016943f1da7fb1be60ff97a414a93cfe7118792d1fd34d21af9e6992e900c040103bf5e44c8b3c0f70a657229'
